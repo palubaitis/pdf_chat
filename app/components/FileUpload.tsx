bc260a48fba975bace334a0e39c051e3fc6bc2b3
Strike7777 <110612063+Strike7777@users.noreply.github.com>
2023-12-12 22:43:03 +0200

    No commit message specified

--- app/components/FileUpload.tsx
+++ app/components/FileUpload.tsx
@@ -1,15 +1,24 @@
+import { Inbox } from "lucide-react";
 import { useDropzone } from "react-dropzone";
 import { toast } from "react-hot-toast";
+import { uploadToS3 } from "~/utils/s3";
 
 export default function FileUpload() {
   const { getRootProps, getInputProps } = useDropzone({
     accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
     onDrop: async (acceptedFiles) => {
+      console.log(acceptedFiles);
       const file = acceptedFiles[0];
       if (file.size > 10 * 1024 * 1024) {
         toast.error("File too large");
         return;
+      }
+      try {
+        const data = await uploadToS3(file);
+        console.log("data: ", data);
+      } catch (error) {
+        console.log(error);
       }
     },
   });
@@ -22,21 +31,10 @@ export default function FileUpload() {
             "border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col",
         })}
       >
-        {/* <input {...getInputProps()} />
-        {uploading || isLoading ? (
-          <>
-          
-            <Loader2 className="h-10 w-10 text-blue-500 animate-spin" />
-            <p className="mt-2 text-sm text-slate-400">
-              Spilling Tea to GPT...
-            </p>
-          </>
-        ) : (
-          <>
-            <Inbox className="w-10 h-10 text-blue-500" />
-            <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
-          </>
-        )} */}
+        <input {...getInputProps()} />
+
+        <Inbox className="w-10 h-10 text-blue-500" />
+        <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
       </div>
     </div>
   );
